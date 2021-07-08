bf885bbecbc0f59fc55357e4e115dbc0052363c5
Justin Seibert <jseibert@appevolve.com>
2021-07-07 18:20:44 -0700

    solid, but needs clean up

--- src/models/kaleidoscope.ts
+++ src/models/kaleidoscope.ts
@@ -8,26 +8,17 @@ import { radian, rotatePoint } from 'src/utils/geometry'
 class Kaleidoscope {
   canvas: Canvas
   hash: string
-  width: number
   height: number
   pixels: number[]
-  slices: number
+  scale: number
   sliceAngle: number
+  slices: number
+  width: number
 
   constructor(canvas: Canvas) {
     this.canvas = canvas
-    // f6108089 fe232c96 4235dac2 10aab04e
-    // f6 10 80 89  fe 23 2c 96 42 35 da c2 10 aa b0 4e
-    // this.hash = '6a'
-    // this.hash = 'f6108089fe232c964235dac210aab04e'
-    // this.hash = '213ff08c960c9d2f7b405886a6e2a98a'
-    this.hash = '5576bb5ec8d4bd6c6f6f178d828c2128' //justin seibert
-    // this.hash = '1ba54aa653fb2448d00b7742adddb39a' //tara chowdhury
-    // 15 6 1 0 8 0 | 15
-    // 246 16 128   | 255
-    // 3937 128     | 4095
-    // this.chunks = []
-    this.width = 256
+    this.hash = '7afa54412789e11a7ade54bc3b3d72acdd263995d4330c36249df3e5bcef0a71' //sha256
+    this.width = 200
     this.height = this.width
     this.scale = this.width * .01
     this.pixels = []
@@ -116,29 +107,28 @@ class Kaleidoscope {
       return i % this.hash.length
     }
 
-    this.hash.split('').forEach((hex:string, index:number) => {
-      // origin of new arc based on +2 indexed nyble
+    for (let index = 0; index < this.hash.length; index += 2) {
+      // origin of new arc based on +3 indexed nyble
       const offset = this.getScale(this.hash[safeIndex(index + 2)])
 
-      // change the reflection pattern every 8 nybles, based on +1 indexed nyble
-      if (index % 8 === 0) {
-        this.slices = (this.dec(this.hash[safeIndex(index + 1)]) * 2) + 4
-        console.log(index, this.slices)
+      // change the reflection pattern every 6 nyble, based on +4 indexed nyble
+      if (index % 4 === 0) {
+        this.slices = this.dec(this.hash[safeIndex(index + 4)]) * 2 + 4
       }
 
       const arcCenter = new Point(
         center.x + offset,
         center.y
       )
-      // radius of new arc based on +0 indexed nyble
-      const radius = this.getScale(hex)
+      // radius of new arc based on +0 indexed byte
+      const radius = this.getScale(this.hash.substring(index, index+2))
 
       const arc = new Arc(
         arcCenter,
         radius
       )
 
-      const increment = radian(this.scale - this.scale/this.width)
+      const increment = radian(this.scale)
       const total = radian(180)
       const maxWidth = center.x + this.width / 2
       let a = 0
@@ -154,7 +144,7 @@ class Kaleidoscope {
         this.addPixel(point)
         this.generateReflections(point, originAngle)
       }
-    })
+    }
   }
 
   generateReflections(point: Point, angle: number) {
