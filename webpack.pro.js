3dfe60783e483d3edefc9f46e6293f153fa0fdda
Justin Seibert <justinseibert@users.noreply.github.com>
2022-03-13 21:20:55 -0700

    feat: create modular build

---
+++ webpack.pro.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+	entry: './dist/tsc/browser.js',
+	mode: 'production',
+	output: {
+		filename: 'index.js',
+		library: {
+			name: 'Metaball',
+			type: 'umd',
+			export: 'default',
+		},
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '',
+	},
+	resolve: {
+		alias: {
+			src: path.resolve(__dirname, 'src'),
+		},
+		extensions: ['.js'],
+	},
+}
