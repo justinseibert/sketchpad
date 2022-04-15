1cee529c03e277f5a79712aca72a8728aafd3e62
Justin Seibert <justinseibert@users.noreply.github.com>
2022-04-14 23:18:32 -0700

    feat: remove custom injection

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,52 +3,46 @@ const webpack = require('webpack')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.ts',
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
-    hot: true,
-  },
-  devtool: 'inline-source-map',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ]
-      },
-    ],
-  },
-  output: {
-    filename: 'bundle.js',
-    library: {
-      name: 'App',
-      type: 'umd',
-      export: 'default',
-    },
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
-  },
-  plugins: [
-    new htmlWebpackPlugin({
-      inject: false,
-      template: path.resolve(__dirname, 'public/index.html'),
-    }),
-  ],
-  resolve: {
-    alias: {
-      'src': path.resolve(__dirname, 'src'),
-    },
-    extensions: [ '.ts', '.js' ],
-  },
+	entry: './src/index.ts',
+	devServer: {
+		host: '0.0.0.0',
+		port: 3000,
+		hot: true,
+	},
+	devtool: 'inline-source-map',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+			},
+		],
+	},
+	output: {
+		filename: 'bundle.js',
+		library: {
+			name: 'App',
+			type: 'umd',
+			export: 'default',
+		},
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '',
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+		}),
+	],
+	resolve: {
+		alias: {
+			src: path.resolve(__dirname, 'src'),
+		},
+		extensions: ['.ts', '.js'],
+	},
 }
