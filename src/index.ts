ac58bee8c97ee5d0a62505cc62ac6c4e3c7d0ff7
Justin Seibert <justinseibert@users.noreply.github.com>
2022-04-16 14:34:38 -0700

    feat: color and shade bubbles

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import './styles/main.scss'
 
 import Canvas from './models/canvas'
-import Point from './models/point'
+import Point, { PointType } from './models/point'
 import Animation from './models/animation'
 
 import { between } from './utils/math'
-import { r360 } from './utils/geometry'
+import { r360, radianBetween, rotatePoint } from './utils/geometry'
+import Color from './models/color'
 
 interface Arc {
-	color: string
+	color: string[]
 	radius: number
 	center: Point
 	slope: Point
@@ -18,6 +19,8 @@ class App {
 	el: HTMLDivElement
 	canvas: Canvas
 	arcs: Arc[]
+	angle: number = 0
+	background: string[]
 	constructor(el: HTMLDivElement) {
 		this.el = el
 	}
@@ -30,7 +33,14 @@ class App {
 			this.canvas.render()
 		}
 		this.canvas.beforeRender = () => {
-			this.canvas.clear()
+			const {
+				bounds: { width, height, bottom, center },
+			} = this.canvas
+
+			const gradient = this.generateGradient({ x: center.x, y: bottom }, Math.max(width, height), this.background)
+
+			this.canvas.ctx.fillStyle = gradient
+			this.canvas.ctx.fillRect(0, 0, this.canvas.bounds.width, this.canvas.bounds.height)
 		}
 
 		window.addEventListener('resize', () => {
@@ -39,6 +49,12 @@ class App {
 		window.addEventListener('mouseup', () => {
 			this.generate()
 		})
+		this.canvas.el.addEventListener('mousemove', (event) => {
+			this.angle = radianBetween(
+				{ x: this.canvas.bounds.center.x, y: this.canvas.bounds.bottom * 1.5 },
+				{ x: event.clientX, y: event.clientY }
+			)
+		})
 
 		const framerate = 100
 		const animate = new Animation(() => {
@@ -46,22 +62,25 @@ class App {
 			this.arcs = this.arcs.map((arc: Arc) => {
 				const {
 					slope,
+					center,
 					center: { x, y },
 					radius,
 					color,
 				} = arc
 				this.canvas.layers.push(() => {
-					this.canvas.ctx.fillStyle = color
+					const gradient = this.generateGradient(center, radius, color)
+
+					this.canvas.ctx.fillStyle = gradient
 					this.canvas.ctx.beginPath()
-					this.canvas.ctx.moveTo(x, y)
 					this.canvas.ctx.arc(x, y, radius, 0, r360)
 					this.canvas.ctx.fill()
+					this.canvas.ctx.closePath()
 				})
 
 				arc.center.x += slope.x
 				arc.center.y += slope.y
 
-				switch (arc.center.atBoundary(this.canvas.bounds)) {
+				switch (arc.center.atEdgeOf(this.canvas.bounds)) {
 					case 'top':
 					case 'bottom':
 						arc.slope.y *= -1
@@ -80,21 +99,44 @@ class App {
 		animate.start()
 	}
 
-	generate() {
-		const arcs = []
+	generateGradient(origin: PointType, radius: number, colors: string[]) {
+		const { x, y } = origin
+		const start = rotatePoint(origin, { x: x - radius, y }, this.angle)
+		const end = rotatePoint(origin, { x: x + radius, y }, this.angle)
+		const gradient = this.canvas.ctx.createLinearGradient(start.x, start.y, end.x, end.y)
+		gradient.addColorStop(0, colors[0] || '')
+		gradient.addColorStop(1, colors[1] || '')
 
+		return gradient
+	}
+
+	generate() {
 		const {
-			bounds: { left, right, top, bottom, width, height },
+			bounds: { left, right, top, bottom, height },
 		} = this.canvas
-		for (let i = 0; i < 500; i++) {
+
+		const backgroundHue = between(0, 360)
+		this.background = [
+			new Color({ h: backgroundHue, s: between(50, 75), l: between(20, 40) }).toString(),
+			new Color({ h: backgroundHue, s: between(75, 100), l: between(80, 100) }).toString(),
+		]
+
+		const arcs = []
+		const max = 0.01
+		const min = max * -1
+		const hue = between(0, 360)
+		const hueRange = 60
+		const total = 600
+		for (let i = 0; i < total; i++) {
 			const radius = between(10, 50)
-			const margin = radius + 10
-			const max = 0.01
-			const min = max * -1
+			const h = between(hue, hue + hueRange)
 			arcs.push({
-				color: '#' + ['', '', ''].map(() => between(0, 255).toString(16)).join(''),
-				center: new Point(between(left + margin, right - margin), between(top + margin, bottom - margin)),
-				slope: new Point(between(width * min, width * max, 2), between(height * min, height * max, 2)),
+				color: [
+					new Color({ h, l: 30 + between(0, 30) }).toString(),
+					new Color({ h, l: 40 + between(40, 50) }).toString(),
+				],
+				center: new Point(between(left - radius, right + radius), between(top + radius, bottom - radius)),
+				slope: new Point(between(-1, 2), between(height * min, height * max, 2)),
 				radius,
 			})
 		}
