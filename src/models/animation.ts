c11a6150b08e766ab3f12e576379d3771157ea7e
Justin Seibert <justinseibert@users.noreply.github.com>
2023-08-12 11:16:20 -0700

    style: autoformats with tabs

--- src/models/animation.ts
+++ src/models/animation.ts
@@ -1,49 +1,49 @@
 class Animation {
-  isActive: boolean
-  fps: number
-  step: () => void
+	isActive: boolean
+	fps: number
+	step: () => void
 
-  constructor(step: () => void, fps: number) {
-    this.isActive = false
-    this.fps = 1000 / fps
-    this.step = step
-  }
+	constructor(step: () => void, fps: number) {
+		this.isActive = false
+		this.fps = 1000 / fps
+		this.step = step
+	}
 
-  public start() {
-    const getFrame = () => {
-      let start = Date.now()
+	public start() {
+		const getFrame = () => {
+			let start = Date.now()
 
-      const onFrame = () => {
-        if (!this.isActive) {
-          return
-        }
+			const onFrame = () => {
+				if (!this.isActive) {
+					return
+				}
 
-        if (Date.now() - start > this.fps) {
-          this.step()
-          getFrame()
-        } else {
-          window.requestAnimationFrame(onFrame)
-        }
-      }
+				if (Date.now() - start > this.fps) {
+					this.step()
+					getFrame()
+				} else {
+					window.requestAnimationFrame(onFrame)
+				}
+			}
 
-      onFrame()
-    }
+			onFrame()
+		}
 
-    this.isActive = true
-    getFrame()
-  }
+		this.isActive = true
+		getFrame()
+	}
 
-  public stop() {
-    this.isActive = false
-  }
+	public stop() {
+		this.isActive = false
+	}
 
-  public animate(shouldRun: boolean) {
-    if (shouldRun) {
-      this.start()
-    } else {
-      this.stop()
-    }
-  }
+	public animate(shouldRun: boolean) {
+		if (shouldRun) {
+			this.start()
+		} else {
+			this.stop()
+		}
+	}
 }
 
 export default Animation
